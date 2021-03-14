6c0f5ae525e158c7cfd27857ecc733ab6b64655d
Maxence Bourgeaux <maxence.bourgeaux@etu.univ-lehavre.fr>
2021-03-14 22:18:51 +0100

    Fonctione draw()

--- src/index.js
+++ src/index.js
@@ -104,3 +104,13 @@ canvas.addEventListener('mouseleave', e => {
     isDrawing = false;
   }
 });
+
+function draw(x1, y1, x2, y2, color) {
+  ctx.beginPath();
+  ctx.strokeStyle = color;
+  ctx.lineWidth = 3;
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
+  ctx.closePath();
+}
